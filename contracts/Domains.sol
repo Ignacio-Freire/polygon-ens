9558bf12a8c8f0a209f5fe844b3f089cf681e184
Ignacio Freire <tkwk@MacBookM1Pro.local>
2022-02-19 22:59:58 -0300

    Customized ENS

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -85,7 +85,7 @@ contract Domains is ERC721URIStorage {
     if (len == 3) {
       return 1 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
     } else if (len == 4) {
-      return 3 * 10**15; // To charge smaller amounts, reduce the decimals. This is 0.3
+      return 1 * 10**15; // To charge smaller amounts, reduce the decimals. This is 0.3
     } else {
       return 1 * 10**14;
     }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,21 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy("ninja");
+    const domainContract = await domainContractFactory.deploy("wtf");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
   
     // CHANGE THIS DOMAIN TO SOMETHING ELSE! I don't want to see OpenSea full of bananas lol
-      let txn = await domainContract.register("banana",  {value: hre.ethers.utils.parseEther('0.1')});
+      let txn = await domainContract.register("poly-ens",  {value: hre.ethers.utils.parseEther('0.1')});
       await txn.wait();
-    console.log("Minted domain banana.ninja");
+    console.log("Minted domain poly-ens.wtf");
   
-    txn = await domainContract.setRecord("banana", "Am I a banana or a ninja??");
+    txn = await domainContract.setRecord("poly-ens", "ENS on Polygon Test");
     await txn.wait();
-    console.log("Set record for banana.ninja");
+    console.log("Set record for poly-ens.wtf");
   
-    const address = await domainContract.getAddress("banana");
-    console.log("Owner of domain banana:", address);
+    const address = await domainContract.getAddress("poly-ens");
+    console.log("Owner of domain poly-ens:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
